0f09baab93de5bca99371352274191a29661888c
Evelyn Snow <git@evelyn.moe>
2019-02-28 17:05:30 +0000

    Specify width in generator

--- intercity_atomic_sleeper/generate.sh
+++ intercity_atomic_sleeper/generate.sh
@@ -1,2 +1,2 @@
-inkscape -z -e out-atomic-sleeper.png atomic-sleeper.svg
+inkscape -w 2000 -z -e out-atomic-sleeper.png atomic-sleeper.svg
 convert out-atomic-sleeper.png PNG8:out-atomic-sleeper.png
